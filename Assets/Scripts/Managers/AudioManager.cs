5c7863c81a2f9c986ab8f9096219db60366920f0
ShadoofJumper <ShadoofJumper@gmail.com>
2019-08-28 22:59:09 +0300

    Complete chapter 10
    Add gui button for control music and add nice transition when change music

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -5,14 +5,57 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour, IGameManager
 {
     public ManagerStatus status { get; private set; }
-    [SerializeField] public AudioSource soundSource;
+    [SerializeField] private AudioSource soundSource;
+
+    [SerializeField] private AudioSource music1Source;
+    [SerializeField] private AudioSource music2Source;
+    private AudioSource _activeMusic;
+    private AudioSource _inactiveMusic;
+    public float crossFadeRate = 1.5f;
+    private bool _crossFading;
 
 
-    public void PlaySound(AudioClip clip)
+    [SerializeField] private string introBGMusic;
+    [SerializeField] private string levelBGMusic;
+
+    private float _musicVolume;
+    public float musicVolume
     {
-        soundSource.PlayOneShot(clip);
+        get
+        {
+            return _musicVolume;
+        }
+        set
+        {
+            _musicVolume = value;
+            if (music1Source != null && !_crossFading)
+            {
+                music1Source.volume = _musicVolume;
+                music2Source.volume = _musicVolume;
+            }
+        }
     }
 
+    public bool musicMute
+    {
+        get
+        {
+            if (music1Source != null)
+            {
+                return music1Source.mute;
+            }
+            return false;
+        }
+        set
+        {
+            if (music1Source != null){
+                music1Source.mute = value;
+                music2Source.mute = value;
+            }
+        }
+    }
+
+   
     public float soundVolume
     {
         get {return AudioListener.volume; }
@@ -27,7 +70,64 @@ public class AudioManager : MonoBehaviour, IGameManager
 
     private NetworkService _networ;
 
-    // here code for controll volume
+    public void PlayIntroMusic()
+    {
+        PlayMusic(Resources.Load("Music/" + introBGMusic) as AudioClip);
+    }
+
+    public void PlayLevelMusic()
+    {
+        PlayMusic(Resources.Load("Music/" + levelBGMusic) as AudioClip);
+
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        if (_crossFading) {return; }
+        StartCoroutine(CrossFadeMusic(clip));
+       // music1Source.clip = clip;
+        //music1Source.Play();
+    }
+
+    private IEnumerator CrossFadeMusic(AudioClip clip)
+    {
+        _crossFading = true;
+
+        _inactiveMusic.clip = clip;
+        _inactiveMusic.volume = 0;
+        _inactiveMusic.Play();
+
+        float scaleRate = crossFadeRate * _musicVolume;
+        while (_activeMusic.volume > 0)
+        {
+            _activeMusic.volume -= scaleRate * Time.deltaTime;
+            _inactiveMusic.volume += scaleRate * Time.deltaTime;
+
+            yield return null;
+        }
+
+        AudioSource temp = _activeMusic;
+
+        _activeMusic = _inactiveMusic;
+        _activeMusic.volume = _musicVolume;
+
+        _inactiveMusic = temp;
+        _inactiveMusic.Stop();
+
+        _crossFading = false;
+    }
+
+    public void StopMusic()
+    {
+        _activeMusic.Stop();
+        _inactiveMusic.Stop();
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        soundSource.PlayOneShot(clip);
+    }
+
 
     public void Startup(NetworkService service)
     {
@@ -36,9 +136,17 @@ public class AudioManager : MonoBehaviour, IGameManager
         Debug.Log(soundSource);
 
         _networ = service;
+        //set music source ignore changes o listener volume
+        music1Source.ignoreListenerVolume = true;
+        music2Source.ignoreListenerVolume = true;
+        music1Source.ignoreListenerPause = true;
+        music2Source.ignoreListenerPause = true;
 
-        // here inizialize sound source
         soundVolume = 1f;
+        musicVolume = 1f;
+
+        _activeMusic = music1Source;
+        _inactiveMusic = music2Source;
 
         status = ManagerStatus.Started;
     }
--- Assets/Scripts/Managers/SettingPopup.cs
+++ Assets/Scripts/Managers/SettingPopup.cs
@@ -7,7 +7,19 @@ public class SettingPopup : MonoBehaviour
 
     [SerializeField] private AudioClip sound;
 
-   public void OnSoundToggle()
+    public void OnMusicToggle()
+    {
+        Managers.Audio.musicMute = !Managers.Audio.musicMute;
+        Managers.Audio.PlaySound(sound);
+    }
+
+    public void OnMusicValue(float volume)
+    {
+        Managers.Audio.musicVolume = volume;
+    }
+
+
+    public void OnSoundToggle()
     {
         Debug.Log("OnSoundToggle");
         Managers.Audio.soundMute = !Managers.Audio.soundMute;
@@ -17,5 +29,21 @@ public class SettingPopup : MonoBehaviour
     public void OnSoundValue(float volume)
     {
         Managers.Audio.soundVolume = volume;
+    }
+
+    public void OnPlayMusic(int selector)
+    {
+        switch (selector)
+        {
+            case 1:
+                Managers.Audio.PlayLevelMusic();
+                break;
+            case 2:
+                Managers.Audio.PlayIntroMusic();
+                break;
+            default:
+                Managers.Audio.StopMusic();
+                break;
+        }
     }
 }
